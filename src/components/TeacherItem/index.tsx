feb80e63ff50f68f1cc27df920b9a1f157d84f4e
Leandro Lopes <lopesleandro.it@gmail.com>
2020-08-08 02:12:00 -0300

    Implementado os componentes reautilizáveis de: - Input - Select - Textarea - TeacherItem Implementado o componente TeacherForm Ajuste dos campos de filtro do componente TeacherList, implementando os campos reutilizáveis e o componente TeacherItem Ajuste do componente TeacherForm, alterando os campos do html puro pelos componentes reutilizáveis Integração com as API's de Back-End de classes e connections para TeacherForm, TeacherList e Landing

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,30 +3,45 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import './styles.css'
+import api from "../../services/api";
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+
+    function createNewConnection() {
+        api.post('connections', {user_id: teacher.id});
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/44936493?s=460&u=24fe0cf0ce37d936a8981cca6a948b748aa3796f&v=4" alt="Leandro Lopes" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Leandro Lopes</strong>
-                    <span>Desenvolvedor Front-End ReactJS e Angular, Desenvolvedor Apex Salesforce</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>Mussum Ipsum, cacilds vidis litro abertis.
-            Si num tem leite então bota uma pinga aí cumpadi!
-            Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!
-            Mauris nec dolor in eros commodo tempor.
-            Aenean aliquam molestie leo, vitae iaculis nisl.
-            Paisis, filhis, espiritis santis.
-                    </p>
+            <p>{teacher.bio}</p>
             <footer>
-                <p>Preço por hora: <strong>R$50,00</strong></p>
-                <button type="button">
+                <p>Preço por hora: <strong>R$ {teacher.cost}</strong></p>
+                <a onClick={createNewConnection} href={`https://wa.me/55${teacher.whatsapp}`} target="_blank">
                     <img src={whatsappIcon} alt="Whatsapp icon" />
-                            Entrar em contato
-                        </button>
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     );
